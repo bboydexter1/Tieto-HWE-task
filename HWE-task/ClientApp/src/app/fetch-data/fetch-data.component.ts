c3e6a327cb3c487a37cb8eeb758a692d6db6c2f5
Sebastian Dyjeta <sebastian-dyjeta@protonmail.com>
2019-07-03 19:16:27 +0200

    data sorting in fetch-data

--- HWE-task/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ HWE-task/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,10 +1,12 @@
 import { Component, Inject } from '@angular/core';
+import { Sort } from '@angular/material/sort';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
+
 export class FetchDataComponent {
   public forecasts: WeatherForecast[];
 
@@ -13,6 +15,29 @@ export class FetchDataComponent {
       this.forecasts = result;
     }, error => console.error(error));
   }
+
+  public sortData(sort: Sort) {
+    const data = this.forecasts.slice();
+    if (!sort.active || sort.direction === '') {
+      this.forecasts = data;
+      return;
+    }
+
+    this.forecasts = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'Date': return compare(a.dateFormatted, b.dateFormatted, isAsc);
+        case 'Temp. (C)': return compare(a.temperatureC, b.temperatureC, isAsc);
+        case 'Temp. (F)': return compare(a.temperatureF, b.temperatureF, isAsc);
+        case 'Summary': return compare(a.summary, b.summary, isAsc);
+        default: return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
 
 interface WeatherForecast {
